ae85a9a3c1a5ba5691733250c957f20aa693e9e3
LuisVar95 <miguel95dev@gmail.com>
2025-11-19 11:28:50 -0600

    Cambios finales antes del deploy

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -147,7 +147,7 @@ export const editarUsuario = async (req, res) => {
 
     try {
         // Verificar que quien hace la petición sea admin
-        if (req.usuario.rol !== 'admin') {
+        if (req.usuario.rol !== 'superadmin') {
             const error = new Error('No tienes permisos para editar usuarios');
             return res.status(403).json({ msg: error.message });
         }
@@ -206,7 +206,7 @@ export const eliminarUsuario = async (req, res) => {
     const {id} = req.params;
 
     try {
-        if(req.usuario.rol !== 'admin') {
+        if(req.usuario.rol !== 'superadmin') {
             const error = new Error('No tienes permisos para eliminar usuarios');
             return res.status(403).json({msg: error.message});
         }
